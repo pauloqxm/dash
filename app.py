660bcf4585b96643a77317e84638190d94befffa
pauloqxm <pauloqxm@gmail.com>
2025-07-15 21:55:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -184,9 +184,10 @@ if not df_filtrado.empty:
         cisternas_layer = folium.FeatureGroup(name="Cisternas")
         for feature in cisternas_geojson["features"]:
             coords = feature["geometry"]["coordinates"]
+            cisternas = feature["properties"].get("Bairro_Loc", "Sem nome")
             folium.Marker(
                 location=[coords[1], coords[0]],
-                popup=folium.Popup(f"Bairro_Loc: {Bairro_Loc}", max_width=200),
+                popup=folium.Popup(f"Bairro_Loc: {cisternas}", max_width=200),
                 tooltip="Cisternas",
                 icon=folium.CustomIcon("https://i.ibb.co/Xkdpcnmx/water-tank.png", icon_size=(20, 20))
             ).add_to(cisternas_layer)
