b386e5532dea39e8dafb0403061125e212548ab7
pauloqxm <pauloqxm@gmail.com>
2025-07-17 09:53:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -181,8 +181,7 @@ if not df_filtrado.empty:
                 location=[row["LATITUDE"], row["LONGITUDE"]],
                 icon=folium.CustomIcon("https://i.ibb.co/My5kq3GH/icons8-fazenda-64.png", icon_size=(25, 25)),
                 popup=folium.Popup(popup_info, max_width=300),
-                tooltip=row["PRODUTO"]
-            ).add_to(m)
+          ).add_to(m)
 
     if show_areas_reforma:
         areas_layer = folium.FeatureGroup(name="Áreas de Reforma")
