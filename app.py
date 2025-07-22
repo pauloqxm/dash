bf72a31f31262b846c8ca34512f2e5cc00ac7a65
pauloqxm <pauloqxm@gmail.com>
2025-07-22 11:42:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ try:
     geojson_files = {
         "distrito": "distrito.geojson",
         "chafarizes": "Chafarizes.geojson",
-        "pocos": "ocos.geojson",
+        "pocos": "Pocos.geojson",
         "sistemas": "Sistemas de Abastecimento.geojson",
         "areas_reforma": "areas_reforma.geojson",
         "distritos_ponto": "distritos_ponto.geojson",
