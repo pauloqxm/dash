d687e8cf8cee57909f4b1cc8311ebb361db7e666
pauloqxm <pauloqxm@gmail.com>
2025-08-04 17:28:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,7 @@ st.set_page_config(page_title="ATLAS SDA - Quixeramobim", layout="wide")
 # Estilos personalizados para a sidebar
 st.markdown("""
     <style>
-        html, body, [data-testid="stAppViewContainer"] > .main {
-            padding: 0 !important;
-            margin: 0 !important;
-        }
+      
         .block-container {
             padding-top: 0rem !important;
         
