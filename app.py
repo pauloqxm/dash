ff99750ca073d7647dcb136483f29b98bada7ede
pauloqxm <pauloqxm@gmail.com>
2025-08-04 15:40:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ except Exception as e:
 # Sidebar
 
 st.sidebar.markdown("""
-    <div style='text-align: center; margin-bottom: 0px;'>
+    <div style='text-align: center; margin-bottom: 25px;'>
         <img src='https://i.ibb.co/jPF2kVzn/brasao.png' width='138' height='100'>
     </div>
 """, unsafe_allow_html=True)
